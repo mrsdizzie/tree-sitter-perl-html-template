0133907025254534198d7c74279b310bfd6378ba
Isla Waters <info@mrsdizzie.com>
2023-08-15 23:50:23 -0400

    Try to add alias

--- grammar.js
+++ grammar.js
@@ -17,7 +17,7 @@ module.exports = grammar({
 
     variable: $ => seq(
       $._start_variable,
-      optional('NAME='), optional('"'), $.parameter_name, optional('"'), 
+      optional('NAME='), optional('"'), $.parameter_name, optional('"'),
       $._close_tag
     ),
 
@@ -30,7 +30,7 @@ module.exports = grammar({
 
     include: $ => seq(
       $._start_include,
-      'NAME=', optional('"'), $.file_name, optional('"'), 
+      'NAME=', optional('"'), $.file_name, optional('"'),
       $._close_tag
     ),
 
@@ -38,10 +38,7 @@ module.exports = grammar({
       $._start_if,
       optional('NAME='), optional('"'), $.parameter_name, optional('"'), '>',
       repeat($.template),
-      optional(seq('<TMPL_ELSE>',
-        optional(seq('NAME=', optional('"'), $.parameter_name, optional('"'))),
-        '>',
-        repeat($.template))),
+      optional(seq($._start_else, repeat($.template))),
       $._close_if
     ),
 
@@ -57,14 +54,15 @@ module.exports = grammar({
     file_name: $ => /[a-zA-Z_][a-zA-Z0-9_]*\.tmpl/,
     content: $ => prec.right(repeat1(choice(/[^<]+|</, '<TMPL'))),
 
-    _start_variable: $ => /<\s*(?:!--\s*)?TMPL_VAR/i,
-    _start_loop: $ => /<\s*(?:!--\s*)?TMPL_LOOP/i,
-    _start_include: $ => /<\s*(?:!--\s*)?TMPL_INCLUDE/i,
-    _start_if: $ => /<\s*(?:!--\s*)?TMPL_IF/i,
-    _start_unless: $ => /<\s*(?:!--\s*)?TMPL_UNLESS/i,
+    _start_variable: $ => alias(/<\s*(?:!--\s*)?TMPL_VAR/i, 'TMPL_VAR'),
+    _start_loop: $ => alias(/<\s*(?:!--\s*)?TMPL_LOOP/i, 'TMPL_LOOP'),
+    _start_include: $ => alias(/<\s*(?:!--\s*)?TMPL_INCLUDE/i, 'TMPL_INCLUDE'),
+    _start_if: $ => alias(/<\s*(?:!--\s*)?TMPL_IF/i, 'TMPL_IF'),
+    _start_else: $ => alias(/<\s*(?:!--\s*)?TMPL_ELSE/i, 'TMPL_ELSE'),
+    _start_unless: $ => alias(/<\s*(?:!--\s*)?TMPL_UNLESS/i, 'TMPL_UNLESS'),
+    _close_loop: $ => alias(/<\s*(?:!--)?\s*\/TMPL_LOOP\s*(?:--)?\s*>/i, '/TMPL_LOOP'),
+    _close_if: $ => alias(/<\s*(?:!--)?\s*\/TMPL_IF\s*(?:--)?\s*>/i, '/TMPL_IF'),
+    _close_unless: $ => alias(/<\s*(?:!--)?\s*\/TMPL_UNLESS\s*(?:--)?\s*>/i, '/TMPL_UNLESS'),
     _close_tag: $ => /\s*(?:--)?\s*>/,
-    _close_loop: $ => /<\s*(?:!--)?\s*\/TMPL_LOOP\s*(?:--)?\s*>/i,
-    _close_if: $ => /<\s*(?:!--)?\s*\/TMPL_IF\s*(?:--)?\s*>/i,
-    _close_unless: $ => /<\s*(?:!--)?\s*\/TMPL_UNLESS\s*(?:--)?\s*>/i,
   }
 });
